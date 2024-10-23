15f8dc898bc4a5675a11e8bafbc0601e057172af
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-23 22:03:08 +0900

    Feat-Add Link in top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <ul className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        <li>
+          <Link href="/page1">Page1</Link>
+        </li>
+        <li>
+          <Link href="/page2">Page 2</Link>
+        </li>
+      </ul>
     </div>
   );
 }
