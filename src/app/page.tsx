7092b182ccd0cfe3a4ff7d54b564e62efefe1515
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-23 22:14:37 +0900

    Feat-Add H1 in top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <ul className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <li>
+    <div className="flex w-full flex-col">
+      <h1 className="m-10 text-4xl">アズールレーン WebTool</h1>
+      <ul className="mx-10 flex w-full flex-col items-center gap-8 sm:items-start">
+        <li className="my-2 w-full">
           <Link href="/exp-calculator">周回経験値計算機</Link>
         </li>
       </ul>
