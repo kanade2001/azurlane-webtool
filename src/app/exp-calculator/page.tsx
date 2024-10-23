59ca14cf08c6bf7ecf32050bcf8430932ef519ab
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 02:02:21 +0900

    Feat-Add H2

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -2,6 +2,10 @@ export default function ExpCalculatorPage() {
   return (
     <div>
       <h1>周回経験値計算機</h1>
+      <h2>Current Exp</h2>
+      <h2>Target Exp</h2>
+      <h2>Preference</h2>
+      <h3>Result</h3>
     </div>
   );
 }
