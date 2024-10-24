31c074c52fb46290ec1153e90dcf832a8921d942
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:46:18 +0900

    Fix-Delete blank

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -6,7 +6,7 @@ export default function ExpCalculatorPage() {
   return (
     <div className="px-5">
       <div className="mx-auto min-h-screen max-w-lg">
-        <h1 className="my-10 text-4xl">周回経験値計算機</h1>{" "}
+        <h1 className="my-10 text-4xl">周回経験値計算機</h1>
         <ExpField label="現在経験値" />
         <ExpField label="目標経験値" />
         <div className="mb-5 rounded-lg border p-2">
