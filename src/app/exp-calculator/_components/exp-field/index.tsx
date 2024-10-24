a57b27666dc126ad1cd19892e8acb441e5f1cd1c
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:43:20 +0900

    Feat-Use numberfield

--- src/app/exp-calculator/_components/exp-field/index.tsx
+++ src/app/exp-calculator/_components/exp-field/index.tsx
@@ -1,3 +1,5 @@
+import { NumberInput } from "@/components/field";
+
 interface ExpFieldProps {
   label: string;
 }
@@ -6,6 +8,11 @@ export default function ExpField({ label }: ExpFieldProps) {
   return (
     <div className="mb-5 rounded-lg border p-2">
       <h2 className="text-xl">{label}</h2>
+      <h3>Level:</h3>
+
+      <NumberInput />
+
+      <h3>Exp:</h3>
     </div>
   );
 }
