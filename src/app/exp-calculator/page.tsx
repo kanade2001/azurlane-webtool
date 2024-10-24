dc9015ad9cd19534d89fe0a82ea9996e6d7b936c
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:11:47 +0900

    Feat-Headerの表示を更新

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -2,11 +2,19 @@ export default function ExpCalculatorPage() {
   return (
     <div className="px-5">
       <div className="mx-auto min-h-screen max-w-lg">
-        <h1>周回経験値計算機</h1>
-        <h2>Current Exp</h2>
-        <h2>Target Exp</h2>
-        <h2>Preference</h2>
-        <h3>Result</h3>
+        <h1 className="my-10 text-4xl">周回経験値計算機</h1>
+        <div className="">
+          <h2 className="mb-5 text-xl">現在経験値</h2>
+        </div>
+        <div className="">
+          <h2 className="mb-5 text-xl">目標経験値</h2>
+        </div>
+        <div className="">
+          <h2 className="mb-5 text-xl">周回設定</h2>
+        </div>
+        <div className="">
+          <h2 className="mb-5 text-xl">計算結果</h2>
+        </div>
       </div>
     </div>
   );
