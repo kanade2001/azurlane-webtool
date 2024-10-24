b5642e190a450d1b65b97322ca5ee1f183106a02
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 23:39:13 +0900

    Feat-NumberInputの動作を実装

--- src/components/field/number-input.tsx
+++ src/components/field/number-input.tsx
@@ -1,5 +1,7 @@
 interface NumberInputProps {
   max_width?: string;
+  value: number;
+  setValue: (value: number) => void;
 }
 
 const NumberInput = (props: NumberInputProps) => {
@@ -13,6 +15,7 @@ const NumberInput = (props: NumberInputProps) => {
           id="decrement-button"
           data-input-counter-decrement="quantity-input"
           className="h-11 rounded-s-lg border border-gray-300 bg-gray-100 p-3 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+          onClick={() => props.setValue(props.value - 1)}
         >
           <svg
             className="h-3 w-3 text-gray-900 dark:text-white"
@@ -31,19 +34,22 @@ const NumberInput = (props: NumberInputProps) => {
           </svg>
         </button>
         <input
-          type="text"
+          type="number"
           id="quantity-input"
           data-input-counter
           aria-describedby="helper-text-explanation"
           className="block h-11 w-full border-x-0 border-gray-300 bg-gray-50 py-2.5 text-center text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
           placeholder="120"
           required
+          value={props.value}
+          onChange={(e) => props.setValue(+e.target.value)}
         />
         <button
           type="button"
           id="increment-button"
           data-input-counter-increment="quantity-input"
           className="h-11 rounded-e-lg border border-gray-300 bg-gray-100 p-3 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+          onClick={() => props.setValue(props.value + 1)}
         >
           <svg
             className="h-3 w-3 text-gray-900 dark:text-white"
