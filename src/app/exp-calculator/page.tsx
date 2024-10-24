fd8eb390f141089ba460c53cb2a4434fc4dcfda8
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:07:23 +0900

    Feat-左右に余白を追加

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -1,11 +1,13 @@
 export default function ExpCalculatorPage() {
   return (
-    <div>
-      <h1>周回経験値計算機</h1>
-      <h2>Current Exp</h2>
-      <h2>Target Exp</h2>
-      <h2>Preference</h2>
-      <h3>Result</h3>
+    <div className="px-5">
+      <div className="mx-auto min-h-screen max-w-lg">
+        <h1>周回経験値計算機</h1>
+        <h2>Current Exp</h2>
+        <h2>Target Exp</h2>
+        <h2>Preference</h2>
+        <h3>Result</h3>
+      </div>
     </div>
   );
 }
