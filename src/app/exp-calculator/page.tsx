4bd85dcf0c18b65018205f352c6aa3c46d64c4d8
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:14:18 +0900

    Feat-枠線を追加

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -3,17 +3,17 @@ export default function ExpCalculatorPage() {
     <div className="px-5">
       <div className="mx-auto min-h-screen max-w-lg">
         <h1 className="my-10 text-4xl">周回経験値計算機</h1>
-        <div className="">
-          <h2 className="mb-5 text-xl">現在経験値</h2>
+        <div className="mb-5 rounded-lg border p-2">
+          <h2 className="text-xl">現在経験値</h2>
         </div>
-        <div className="">
-          <h2 className="mb-5 text-xl">目標経験値</h2>
+        <div className="mb-5 rounded-lg border p-2">
+          <h2 className="text-xl">目標経験値</h2>
         </div>
-        <div className="">
-          <h2 className="mb-5 text-xl">周回設定</h2>
+        <div className="mb-5 rounded-lg border p-2">
+          <h2 className="text-xl">周回設定</h2>
         </div>
-        <div className="">
-          <h2 className="mb-5 text-xl">計算結果</h2>
+        <div className="mb-5 rounded-lg border p-2">
+          <h2 className="text-xl">計算結果</h2>
         </div>
       </div>
     </div>
