4ace9c6e1c80a68e5166ee70780f88c458d84803
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:19:47 +0900

    Feat-Import ExpField in page

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ExpField } from "./_components";
+
 export default function ExpCalculatorPage() {
   return (
     <div className="px-5">
@@ -7,6 +9,7 @@ export default function ExpCalculatorPage() {
         <h1 className="my-10 text-4xl">周回経験値計算機</h1>
         <div className="mb-5 rounded-lg border p-2">
           <h2 className="text-xl">現在経験値</h2>
+          <ExpField />
         </div>
         <div className="mb-5 rounded-lg border p-2">
           <h2 className="text-xl">目標経験値</h2>
