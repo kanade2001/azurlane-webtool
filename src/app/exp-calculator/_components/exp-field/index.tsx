d59fd7d06da3c3b4b4359e599e3306b39991ee31
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-24 22:57:27 +0900

    Feat-Gridに整列

--- src/app/exp-calculator/_components/exp-field/index.tsx
+++ src/app/exp-calculator/_components/exp-field/index.tsx
@@ -8,11 +8,12 @@ export default function ExpField({ label }: ExpFieldProps) {
   return (
     <div className="mb-5 rounded-lg border p-2">
       <h2 className="text-xl">{label}</h2>
-      <h3>Level:</h3>
-
-      <NumberInput />
-
-      <h3>Exp:</h3>
+      <div className="grid grid-cols-[1fr_3fr] items-center justify-items-start gap-4">
+        <h3> Level:</h3>
+        <NumberInput />
+        <h3> Exp:</h3>
+        <NumberInput />
+      </div>
     </div>
   );
 }
