14827d11e3b2bfbd6f6492d48a3201a11efe4d73
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-28 23:19:33 +0900

    Feat-スライドの挙動を実装

--- src/components/field/slide-input.tsx
+++ src/components/field/slide-input.tsx
@@ -1,8 +1,17 @@
 interface SlideInputProps {
   value: number;
-  min_value?: string | number;
-  max_value?: string | number;
+  onChange: (value: number) => void;
+  min_value: number;
+  max_value: number;
 }
+
+const CalcRatio = (value: number, min: number, max: number) => {
+  return ((value - min) / (max - min)) * 100;
+};
+
+const CalcValue = (ratio: number, min: number, max: number) => {
+  return min + (max - min) * (ratio / 100);
+};
 
 const SlideInput: React.FC<SlideInputProps> = (props: SlideInputProps) => {
   return (
@@ -10,8 +19,13 @@ const SlideInput: React.FC<SlideInputProps> = (props: SlideInputProps) => {
       <input
         id="slide-input"
         type="range"
-        value={props.value}
+        value={CalcRatio(props.value, props.min_value, props.max_value)}
         className="h-2 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
+        onChange={(e) =>
+          props.onChange(
+            CalcValue(+e.target.value, props.min_value, props.max_value),
+          )
+        }
       />
     </div>
   );
