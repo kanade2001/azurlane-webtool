cba112c93d967411b23093b2530ee1b7088366ba
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-28 22:59:22 +0900

    Add-SlideInput

---
+++ src/components/field/slide-input.tsx
@@ -0,0 +1,10 @@
+interface SlideInputProps {
+  min_value: string | number;
+  max_value: string | number;
+}
+
+const SlideInput: React.FC<SlideInputProps> = (props: SlideInputProps) => {
+  return <></>;
+};
+
+export default SlideInput;
