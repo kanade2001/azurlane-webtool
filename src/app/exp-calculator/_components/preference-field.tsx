3d37493a20a2215b41472407c3e6d9acce6484dc
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-29 01:54:40 +0900

    Feat-カテゴリ選択フィールドを追加

--- src/app/exp-calculator/_components/preference-field.tsx
+++ src/app/exp-calculator/_components/preference-field.tsx
@@ -11,13 +11,22 @@ export default function PreferenceField(props: PreferenceFieldProps) {
       <h2 className="text-xl">{props.label}</h2>
       <div className="flex flex-col items-center gap-4">
         <h3>海域</h3>
-        <SelectInput
-          id="area"
-          options={[
-            { value: "1", label: "1-1" },
-            { value: "2", label: "1-2" },
-          ]}
-        />
+        <div className="grid w-full grid-cols-2 gap-2">
+          <SelectInput
+            id="area"
+            options={[
+              { value: "Normal", label: "ノーマル" },
+              { value: "Hard", label: "ハード" },
+            ]}
+          />
+          <SelectInput
+            id="area"
+            options={[
+              { value: "1", label: "1-1" },
+              { value: "2", label: "1-2" },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
