5ffb05fbad126e0f3e5a6c5a7c9fe442b25667f5
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-29 21:49:54 +0900

    Fix-不要なフィールドを削除

--- src/app/exp-calculator/page.tsx
+++ src/app/exp-calculator/page.tsx
@@ -31,13 +31,6 @@ export default function ExpCalculatorPage() {
           target_exp={targetExp}
           exp={exp}
         />
-        <div className="mb-5 rounded-lg border p-2">
-          <h2 className="text-xl">計算結果</h2>
-          必要経験値
-          {targetExp - currentExp}
-          {targetExp - currentExp / exp[0]}
-          {targetExp - currentExp / exp[1]}
-        </div>
       </div>
     </div>
   );
