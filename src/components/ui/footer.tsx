24201b45393f11afc8dd91084c3c43ed55609632
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-30 00:03:26 +0900

    Feat-Add Footer

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="flex w-full items-center justify-between bg-gray-300 p-2 dark:bg-gray-700">
+      <p>Azurlane-Webtool</p>
+      <p>©kanade2001</p>
+    </footer>
+  );
+};
+
+export default Footer;
