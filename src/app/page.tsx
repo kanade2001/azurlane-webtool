dbe4eedd1cd7aa06e3fecc9bbe8772b7338be5d7
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-31 00:43:07 +0900

    Feat-トップページを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
 
+import { Header, Footer } from "@/components/ui";
+
 export default function Home() {
   return (
-    <div className="flex w-full flex-col">
-      <h1 className="m-10 text-4xl">アズールレーン WebTool</h1>
-      <ul className="mx-10 flex w-full flex-col items-center gap-8 sm:items-start">
-        <li className="my-2 w-full">
-          <Link href="/exp-calculator">周回経験値計算機</Link>
-        </li>
-      </ul>
+    <div className="flex min-h-screen w-full flex-col items-center">
+      <Header />
+      <div className="flex w-full max-w-lg grow flex-col">
+        <h1 className="my-10 text-center text-4xl">アズールレーン WebTool</h1>
+        <ul className="flex w-full flex-col items-center gap-8">
+          <li className="my-2 w-full rounded-lg border border-gray-500 p-2">
+            <Link href="/exp-calculator" className="flex flex-col gap-4">
+              <h2 className="text-xl">周回経験値計算機</h2>
+              <p>
+                現在のレベル・経験値と目標のレベル・経験値から、必要な周回数を計算します。
+              </p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Footer />
     </div>
   );
 }
