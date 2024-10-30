7b0741accc580fd4c8b87e67a25bdf72d744259d
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-31 00:35:16 +0900

    Fix-中央寄せ

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex w-full items-center justify-end bg-gray-300 p-2 dark:bg-gray-700">
+    <footer className="flex w-full items-center justify-center bg-gray-300 p-2 dark:bg-gray-700">
       <p>kanade2001</p>
     </footer>
   );
